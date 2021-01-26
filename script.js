3262cd0c24d4c1688a37d4f2f2c08fad739b3ebb
danielstos <daniel.jose.stos@gmail.com>
2021-01-25 21:24:04 -0300

    foi acrescentado pontuação aleatorio , ealterações de cores

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+const score = document.querySelector('.score');
+
+let pontos = 0;
 let isJumping = false;
 let position = 0;
 
@@ -11,17 +14,18 @@ function handleKeyUp(event) {
     }
 }
 
-function jump() {  
+function jump() {
     isJumping = true;
     let upInterval = setInterval(() => {
-        if (position >= 150) {
+        if (position >= 250) {
             clearInterval(upInterval);
             //descendo
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
                     isJumping = false;
-                } else {
+                }
+                else {
                     position -= 20;
                     dino.style.bottom = position + 'px'
                 }
@@ -33,30 +37,38 @@ function jump() {
         // subindo
         position += 20;
         dino.style.bottom = position + 'px'
-    }, 20)
+    }, 30)
 }
 
 function creatCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
     let randomTime = Math.random() * 6000;
+    
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
 
     let leftInterval = setInterval(() => {
-        if (cactusPosition < -60) {
+        if (cactusPosition < -80) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
-        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+        } else if (cactusPosition > 0 && cactusPosition < 80 && position < 60) {
             clearInterval(leftInterval);
-            document.body.innerHTML= '<h1 class="game-over">Fim de jogo</h1>';
-            }else{
-                cactusPosition -= 10;
-                cactus.style.left = cactusPosition + 'px';                
-            }
-    }, 20);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo :( score:'+pontos+'</h1>';
+       
+
+        } else if (cactusPosition > 0 && cactusPosition < 80 && position > 180) {
+           pontos+=1;            
+            
+        }
+        else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+        score.textContent = "score:" + pontos;
+    }, 30);
 
     setTimeout(creatCactus, randomTime)
 }
